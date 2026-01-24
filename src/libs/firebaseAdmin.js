85561ce342ce900f988f8e8caad4036db07a784e
rumbiiha <rswaib@gmail.com>
2026-01-24 22:43:41 +0300

    improve .env

--- src/libs/firebaseAdmin.js
+++ src/libs/firebaseAdmin.js
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
         // Handle private key newlines in env vars
         privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
